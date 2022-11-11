c4afa9c87b777d58446f2b3176215c757859e357
Tallisson Souza <tallissons@gmail.com>
2022-11-11 08:39:20 -0300

    Install Jetstream

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,14 @@ Route::get('/', function () {
 });
 
 
-Route::get('/tweets', ShowTweets::class);
+Route::get('/tweets', ShowTweets::class)->middleware('auth');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});

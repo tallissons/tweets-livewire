18be3e7c4e4cf26619fdabb57d4ca0c0fcfa0ad8
Tallisson Souza <tallissons@gmail.com>
2022-11-10 23:28:29 -0300

    Create Tweet

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -3,6 +3,10 @@
 
     <p>{{$message}}</p>
 
+    <form action="" method="post" wire:submit.prevent="create">
+        <input type="text" name="message" id="message" cols="30" rows="10" wire:model="message">
+        <button type="submit">Criar Tweet</button>
+    </form>
     <textarea name="message" id="message" cols="30" rows="10" wire:model="message"></textarea>
 
     <br>
